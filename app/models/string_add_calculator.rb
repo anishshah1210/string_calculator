bef297869ded5bc1539b6c8b9583a93d5d84e650
Anish Shah <anish@atharvasystem.com>
2025-01-28 08:27:59 +0530

    Write a code to pass testcase of new line character in string

--- app/models/string_add_calculator.rb
+++ app/models/string_add_calculator.rb
@@ -1,6 +1,8 @@
 class StringAddCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    numbers = numbers.gsub("\n", ",")
+    p numbers
     return numbers.split(',').map(&:to_i).sum
   end
 end
