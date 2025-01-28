c49ad39000968094c7470a8d383732e60874e4c1
Anish Shah <anish@atharvasystem.com>
2025-01-28 09:56:40 +0530

    write a code to pass multi delimetor

--- app/models/string_add_calculator.rb
+++ app/models/string_add_calculator.rb
@@ -2,28 +2,30 @@ class StringAddCalculator
   def add(numbers)
     return 0 if numbers.empty?
     delimiters =[","]
-    if numbers.start_with?('//')
+    if numbers.start_with?("//[")
+      delimiters = numbers.scan(/\[(.*?)\]/).flatten
+      numbers = numbers.split("\n", 2)[1]
+      p numbers
+      p "------------"
+    elsif numbers.start_with?('//')
       delimiters = [numbers[2]]
       numbers = numbers.split("\n", 2)[1]
       p numbers
     else
       numbers = numbers.gsub("\n", ",")
     end  
+    p numbers
     delimiter_pattern = "[#{delimiters.join}]+"
     
     numbers = numbers.split(Regexp.new(delimiter_pattern))
 
-    # nagative
-    p "ff"
-    p numbers
-    p "hh"
     negative_numbers = numbers.select { |num| num.to_i < 0 }
     if negative_numbers.any?
       raise "Negative numbers not allowed: #{negative_numbers.join(', ')}"
     end
     numbers  = numbers.select { |num| num.to_i < 1001 }
 
-    
+
     return numbers.map(&:to_i).sum
   end
 end
