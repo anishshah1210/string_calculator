16c4b0898def091c2d744f7e3dd7d4c29d53895b
Anish Shah <anish@atharvasystem.com>
2025-01-28 08:36:47 +0530

    Test cases for string start with //

--- spec/models/string_add_calculator_spec.rb
+++ spec/models/string_add_calculator_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe StringAddCalculator, type: :model do
       calc = StringAddCalculator.new
       expect(calc.add("1\n2,3")).to eq(6)
     end
-
+    
+    it "return sum when custom delimiter in string" do
+      calc = StringCalculator.new
+      expect(calc.add("//;\n1;2")).to eq(3)
+    end
+    
   end
 end
